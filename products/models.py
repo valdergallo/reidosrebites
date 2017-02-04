d541b991df1c5b048d3ab0befd5618b1c6a2cdfa
Valder Gallo <valdergallo@gmail.com>
2017-02-04 11:45:44 -0200

    updat names

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ from django.contrib.sitemaps import ping_google
 
 class Product(models.Model):
     image = models.ImageField(upload_to='products/')
-    name = models.CharField(max_length=150)
+    title = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
     is_draft = models.BooleanField(default=False)
     price = models.DecimalField(max_digits=5, decimal_places=2)
