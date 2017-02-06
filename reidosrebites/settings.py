49a4323eef45489aaf7b78bfb05791a0827ba86c
Valder Gallo <valdergallo@gmail.com>
2017-02-06 20:11:58 -0200

    update test config

--- reidosrebites/settings.py
+++ reidosrebites/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'reidosrebites.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
